e2019de0f5d2055bce26b48018e363145b642cac
alinc66 <44928915+alinc66@users.noreply.github.com>
2023-06-12 15:26:22 +0300

    Update larc_app.py

--- larc_app.py
+++ larc_app.py
@@ -69,7 +69,7 @@ bad = Image.open('0.png')
 good = Image.open('1.png')
 
 st.title('Neoadjuvant Chemoradiotherapy LARC Classification App')
-st.image("""https://www.thestreet.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTY4NjUwNDYyNTYzNDExNTkx/why-dominion-diamonds-second-trip-to-the-block-may-be-different.png""")
+st.image(head_image.png)
 st.header('Enter the predictor variables:')
 
 sex = st.selectbox('Sex:', ['Male', 'Female'],index=0)
